c13c84dfa11d57fbfce3bba73533b31056cae1a8
Vishal Kumawat <vishal.kumawat@appinventiv.com>
2024-08-08 11:39:35 +0530

    ts error solved

--- src/App.tsx
+++ src/App.tsx
@@ -41,10 +41,9 @@ function App() {
       idx === index
         ? {
             ...todo,
-            // todoStatus: todo.todoStatus === "incomplete" ? "completed" : "incomplete",
             todoStatus: (todo.todoStatus === "incomplete"
               ? "completed"
-              : "incomplete") as "incomplete" | "completed",
+              : "incomplete") as TodoType["todoStatus"],
           }
         : todo
     );
